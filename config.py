2fe5c7a68db03507eabe6ed7459b6da324c2eff8
Ganang1312 <hoangduong.bka@gmail.com>
2025-08-31 14:22:58 +0700

    Update config.py

--- config.py
+++ config.py
@@ -20,5 +20,7 @@ CLIENT = gspread.authorize(CREDS)
 SHEET_NAME = 'DATA REATIME'
 WORKSHEET_NAME_USERS = 'allowed_users'
 WORKSHEET_TRACKER_NAME = 'task_tracker'
-# SỬA LỖI: Bổ sung dòng còn thiếu dưới đây
 WORKSHEET_NAME = 'chi_tiet_cum'
+# SỬA LỖI: Bổ sung dòng còn thiếu dưới đây
+WORKSHEET_SCHEDULES_NAME = 'schedules'
+
